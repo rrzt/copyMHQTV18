3ffd214a1eddac94ab7a3d2553671f2d4e45be51
138001380000 <842250464@qq.com>
2022-09-20 06:02:16 +0000

    上传文件至 'py'

--- py/py_ali.py
+++ py/py_ali.py
@@ -408,7 +408,7 @@ class Spider(Spider):  # 元类 默认的元类 type
         url = 'https://api.aliyundrive.com/token/refresh'
         if len(self.authorization) == 0 or self.timeoutTick - self.localTime <= 600:
             form = {
-                'refresh_token': '569a2e9c46034e369260f2d053e3cda0'
+                'refresh_token': '59744816075f4729bc00cd23b98e3447'
             }
             rsp = requests.post(url, json=form, headers=self.header)
             if rsp.status_code == 200:
