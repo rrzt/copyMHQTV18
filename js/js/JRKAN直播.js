257088c8a07e8cb294128c7618cb016511d3aa1b
138001380000 <842250464@qq.com>
2022-12-12 07:58:48 +0000

    上传文件至 'js/js'

--- js/js/JRKAN直播.js
+++ js/js/JRKAN直播.js
@@ -11,7 +11,7 @@
 
 var rule = {
     title:'JRKAN直播',
-    host:'http://www.jrskan.com',
+    host:'http://www.jrkan365.com/?lan=1',
 	// JRKAN备用域名:jrkan365.com / jrkankan.com / jryyds.com / jrsbxj.com
 	// JRKAN网址发布:qiumi1314.com
     url:'/fyclass',
@@ -19,8 +19,8 @@ var rule = {
     searchable:0,
     quickSearch:0,
     class_name:'全部',
-    //class_url:'/',
-    class_url:'?live',
+    class_url:'/',
+    //class_url:'?live',
     headers:{
         'User-Agent':'MOBILE_UA'
     },
@@ -31,9 +31,9 @@ var rule = {
     double:false,
     推荐:'*',
     // 一级播放线路x3 可自行切换
-    // 一级:'.loc_match .d-touch;li&&Text;img&&src;.lab_time&&Text;a:eq(0)&&href',//play.sportsteam365.com
+    //一级:'.loc_match .d-touch;li&&Text;img&&src;.lab_time&&Text;a:eq(0)&&href',//play.sportsteam365.com
     一级:'.loc_match:eq(2) ul;li:gt(1):lt(4)&&Text;img&&src;li:lt(2)&&Text;a:eq(1)&&href',//play.sportsteam333.com
-    // 一级:'.loc_match .d-touch;li&&Text;img&&src;.lab_time&&Text;a:eq(2)&&href',//play.sportsteam666.com
+    //一级:'.loc_match .d-touch;li&&Text;img&&src;.lab_time&&Text;a:eq(2)&&href',//play.sportsteam666.com
     二级:{title:'.sub_list li:lt(2)&&Text;.sub_list li:eq(0)&&Text',img:'img&&src',desc:';;;.lab_team_home&&Text;.lab_team_away&&Text',content:'.sub_list ul&&Text',tabs:'',tab_text:'',lists:'.sub_channel a',list_text:'a&&Text',list_url:'a&&data-play'},
     搜索:'',
 }
